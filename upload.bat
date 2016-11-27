15fb31b6f5db2b027ae26a84467f4893b0bc33bc
Javi Moreno <javi.moreno@capside.com>
2016-11-27 15:57:46 +0100

    Comprimida css antes de subirlo y fijado cache control.

--- upload.bat
+++ upload.bat
@@ -1,3 +1,3 @@
 rm public/ -r
 hugo
-aws s3 cp --acl public-read --storage-class REDUCED_REDUNDANCY --recursive public/ s3://www.programar.cloud
+aws s3 cp --cache-control max-age=3600 --acl public-read --storage-class REDUCED_REDUNDANCY --recursive public/ s3://www.programar.cloud
